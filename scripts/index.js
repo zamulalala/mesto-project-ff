29a55be4dbe58c483e15dc45a612af79548029ca
zamulalala <ritazamula2001@gmail.com>
2023-11-14 02:17:07 +0300

    iteration 1

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,22 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const placesList = document.querySelector('.places__list');
+const cardTemplate = document.querySelector('#card-template').content;
+
+function delCard (el) {
+  el.closest('.card').remove();
+}
+
+initialCards.forEach(function (el) {
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+
+  cardElement.querySelector('.card__image').src = el.link;
+  cardElement.querySelector('.card__title').textContent = el.name;
+
+  const closeButton = cardElement.querySelector('.card__delete-button'); // Найти кнопку внутри каждой карточки
+  closeButton.addEventListener('click', () => delCard(closeButton));
+
+  placesList.append(cardElement);
+})
