2487d1b002619359a3a02f29281aa28879884706
zamulalala <ritazamula2001@gmail.com>
2023-11-14 03:01:06 +0300

    iteration 1.1

--- scripts/cards.js
+++ scripts/cards.js
@@ -2,25 +2,31 @@ const initialCards = [
     {
       name: "Архыз",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+      alt: "Горы Архыза, на которых лежит снег",
     },
     {
       name: "Челябинская область",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+      alt: "Лес Челябинской области отражается в озере среди заснеженных гор",
     },
     {
       name: "Иваново",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+      alt: "Серые панельные дома в пасмурный день",
     },
     {
       name: "Камчатка",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+      alt: "Растения, проростающие в земле у подножья гор Камчатки",
     },
     {
       name: "Холмогорский район",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+      alt: "Рельсы разделяют лес, изображены в перспективе, стремяься в даль",
     },
     {
       name: "Байкал",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+      alt: "Лед и горы – зимний Байкал",
     }
 ];
--- scripts/index.js
+++ scripts/index.js
@@ -11,18 +11,21 @@
 const placesList = document.querySelector('.places__list');
 const cardTemplate = document.querySelector('#card-template').content;
 
-function delCard (el) {
-  el.closest('.card').remove();
+function createCard (el, deleteCard) {
+  cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  cardElement.querySelector('.card__image').src = el.link;
+  cardElement.querySelector('.card__image').alt = el.alt;
+  cardElement.querySelector('.card__title').textContent = el.name;
+  const closeButton = cardElement.querySelector('.card__delete-button');
+  closeButton.addEventListener('click', deleteCard);
+  return cardElement;
+}
+
+function deleteCard (evt) {
+  evt.target.closest('.card').remove();
 }
 
 initialCards.forEach(function (el) {
-  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
-
-  cardElement.querySelector('.card__image').src = el.link;
-  cardElement.querySelector('.card__title').textContent = el.name;
-
-  const closeButton = cardElement.querySelector('.card__delete-button'); // Найти кнопку внутри каждой карточки
-  closeButton.addEventListener('click', () => delCard(closeButton));
-
+  const cardElement = createCard(el, deleteCard);
   placesList.append(cardElement);
 })
